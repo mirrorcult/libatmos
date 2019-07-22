f634e0031881b0229aab60259ae9bda38e87b04d
cyclowns <notzombiedude@gmail.com>
2019-07-22 00:38:31 -0700

    update license + basegas.rs statics

--- src/atmospherics/gasmixtures/basegas.rs
+++ src/atmospherics/gasmixtures/basegas.rs
@@ -1,13 +1,13 @@
 /// Struct used to identify and organize different gases by name.
-pub struct BaseGas<'a> {
+pub struct BaseGas {
     /// Short identifier
-    pub id: &'a str,
+    pub id: String,
     /// More formal, user-friendly name
-    pub name: &'a str,
+    pub name: String,
     /// Amount of energy required to heat a gas up by 1 deg. celsius.
-    /// Higher => harder to cool/heat
+    /// Higher => harder to cool/heat.
     /// Lower => easier to cool/heat
     pub specific_heat: u32,
     /// Multiplier for how much a gas accelerates a fusion reaction
-    pub fusion_power: u32
+    pub fusion_power: i32
 }
