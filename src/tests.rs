615d4ff04e171227e8db617dd388b0d55468117a
cyclowns <notzombiedude@gmail.com>
2019-07-24 19:22:39 -0700

    fix create_gas_mixture test

--- src/tests.rs
+++ src/tests.rs
@@ -7,7 +7,7 @@ use gasmixtures::*;
 fn create_gas_mixture() {
     let gas_vec = vec![&gases::BZ, &gases::MIASMA];
     let mole_vec = vec![50.0, 500.5];
-    let mix = gasmixture::GasMixture::from_vecs(gas_vec, mole_vec, 273.15, 70);
+    let mix = gasmixture::GasMixture::from_vecs(gas_vec, mole_vec, 273.15, 70).unwrap();
     assert_eq!(mix.temperature, 273.15);
     assert_eq!(mix.volume, 70);
 }
